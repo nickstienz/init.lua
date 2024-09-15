44b7aa7a8c73b0e325a75bb67069bf79c6f8baf9
Nicholas Stienz <nickstienz@gmail.com>
2024-09-15 17:41:01 -0500

    Idk what I did

---
+++ lua/nick/plugins/treesitter.lua
@@ -0,0 +1,54 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+        "windwp/nvim-ts-autotag",
+    },
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
+
+        treesitter.setup({
+            highlight = {
+                enable = true,
+            },
+
+            indent = {
+                enable = true,
+            },
+
+            autotag = {
+                enable = true,
+            },
+
+            ensure_installed = {
+                "asm",
+                "bash",
+                "c",
+                "cmake",
+                "cpp",
+                "dockerfile",
+                "java",
+                "json",
+                "latex",
+                "llvm",
+                "lua",
+                "make",
+                "markdown",
+                "markdown_inline",
+                "regex",
+                "rust",
+                "toml",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
+    end,
+}
