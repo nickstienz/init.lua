ea838a9d2510afdd2f544d08c9172e2ccac2dd9b
Nicholas Stienz <nickstienz@gmail.com>
2024-09-16 13:13:35 -0500

    Changed undotree and fugitive to VeryLazy, <50ms startup YUMMY

--- lua/nick/plugins/vim-fugitive.lua
+++ lua/nick/plugins/vim-fugitive.lua
@@ -1,5 +1,6 @@
 return {
     "tpope/vim-fugitive",
+    event = "VeryLazy",
     config = function()
         local keymap = vim.keymap
         keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Open git with fugitive" })
