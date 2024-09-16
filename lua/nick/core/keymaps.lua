f77103d7949c3e9bcecc30e75b10d3cfd6f4c461
Nicholas Stienz <nickstienz@gmail.com>
2024-09-15 21:19:45 -0500

    Auto format on save and C-s to save

--- lua/nick/core/keymaps.lua
+++ lua/nick/core/keymaps.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Saves current buffer" })
+
 keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Opens netrw" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
@@ -10,4 +12,3 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
-
