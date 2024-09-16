1edf2f47e853d17122447ddb258c60a9f8fcb8b2
Nicholas Stienz <nickstienz@gmail.com>
2024-09-16 13:28:01 -0500

    Changed clear highlight to <leader>ch

--- lua/nick/core/keymaps.lua
+++ lua/nick/core/keymaps.lua
@@ -6,7 +6,7 @@ keymap.set("n", "<C-s>", ":w<CR>", { desc = "Saves current buffer" })
 
 keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Opens netrw" })
 
-keymap.set("n", "<escape><escape>", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>ch", ":nohl<CR>", { desc = "Clear search highlights" })
 
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
