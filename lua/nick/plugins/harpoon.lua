c2e917e202bf3d08a66fc71db0da2e4726daec0f
Nicholas Stienz <nickstienz@gmail.com>
2024-09-16 12:57:52 -0500

    Added helper function to harpoon to use

--- lua/nick/plugins/harpoon.lua
+++ lua/nick/plugins/harpoon.lua
@@ -12,20 +12,17 @@ return {
         })
 
         local keymap = vim.keymap
+
+        function map(number)
+            keymap.set("n", ("<leader>"..number), function() harpoon:list():select(number) end, { desc = ("Open harpoon "..number) })
+            keymap.set("n", ("<leader>h"..number), function() harpoon:list():replace_at(number) end, { desc = ("Set harpoon "..number.." to current buffer") })
+        end
+
         keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Add file to harpoon" })
         keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open harpoon menu" })
 
-        keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Open harpoon 1" })
-        keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Open harpoon 2" })
-        keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Open harpoon 3" })
-        keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Open harpoon 4" })
-        keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Open harpoon 5" })
-
-
-        keymap.set("n", "<leader>h1", function() harpoon:list():replace_at(1) end, { desc = "Set harpoon 1 to current buffer" })
-        keymap.set("n", "<leader>h2", function() harpoon:list():replace_at(2) end, { desc = "Set harpoon 2 to current buffer" })
-        keymap.set("n", "<leader>h3", function() harpoon:list():replace_at(3) end, { desc = "Set harpoon 3 to current buffer" })
-        keymap.set("n", "<leader>h4", function() harpoon:list():replace_at(4) end, { desc = "Set harpoon 4 to current buffer" })
-        keymap.set("n", "<leader>h5", function() harpoon:list():replace_at(5) end, { desc = "Set harpoon 5 to current buffer" })
+        for x = 1, 5 do
+            map(x)
+        end
     end,
 }
