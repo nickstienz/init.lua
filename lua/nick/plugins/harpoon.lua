303521d43e90baaa38e84ec33fff425b8f878836
Nicholas Stienz <nickstienz@gmail.com>
2024-09-16 12:47:55 -0500

    Updated harpoon to harpoon2 and changed binds

--- lua/nick/plugins/harpoon.lua
+++ lua/nick/plugins/harpoon.lua
@@ -1,17 +1,31 @@
 return {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
+    event = "VeryLazy",
     config = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
+        local harpoon = require("harpoon")
+        harpoon:setup({
+            settings = {
+                save_on_toggle = true,
+            }
+        })
 
         local keymap = vim.keymap
-        keymap.set("n", "<leader>a", mark.add_file, { desc = "Added file to harpoon" })
-        keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Open harpoon menu" })
-        
-        keymap.set("n", "<leader>h", function() ui.nav_file(1) end, { desc = "Navigate to file 1" })
-        keymap.set("n", "<leader>j", function() ui.nav_file(2) end, { desc = "Navigate to file 2" })
-        keymap.set("n", "<leader>k", function() ui.nav_file(3) end, { desc = "Navigate to file 3" })
-        keymap.set("n", "<leader>l", function() ui.nav_file(4) end, { desc = "Navigate to file 4" })
+        keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Add file to harpoon" })
+        keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open harpoon menu" })
+
+        keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Open harpoon 1" })
+        keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Open harpoon 2" })
+        keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Open harpoon 3" })
+        keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Open harpoon 4" })
+        keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Open harpoon 5" })
+
+
+        keymap.set("n", "<leader>h1", function() harpoon:list():replace_at(1) end, { desc = "Set harpoon 1 to current buffer" })
+        keymap.set("n", "<leader>h2", function() harpoon:list():replace_at(2) end, { desc = "Set harpoon 2 to current buffer" })
+        keymap.set("n", "<leader>h3", function() harpoon:list():replace_at(3) end, { desc = "Set harpoon 3 to current buffer" })
+        keymap.set("n", "<leader>h4", function() harpoon:list():replace_at(4) end, { desc = "Set harpoon 4 to current buffer" })
+        keymap.set("n", "<leader>h5", function() harpoon:list():replace_at(5) end, { desc = "Set harpoon 5 to current buffer" })
     end,
 }
