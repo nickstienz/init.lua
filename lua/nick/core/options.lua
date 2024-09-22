d310df88f46bb09f0abc66efa955afcbd6e88d5e
Nicholas Stienz <nickstienz@gmail.com>
2024-09-22 16:13:08 -0500

    Changed options a bit (small changes)

--- lua/nick/core/options.lua
+++ lua/nick/core/options.lua
@@ -4,9 +4,11 @@ opt.relativenumber = true
 opt.number = true
 
 opt.tabstop = 4
+opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
-opt.autoindent = true
+
+opt.smartindent = true
 
 opt.wrap = false
 
