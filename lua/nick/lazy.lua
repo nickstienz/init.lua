4d6eddc41027398c7a0185a645509f3d1043e248
Nicholas Stienz <nickstienz@gmail.com>
2024-09-21 23:05:03 -0500

    Started adding LSP support

--- lua/nick/lazy.lua
+++ lua/nick/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("nick.plugins", {
+require("lazy").setup({ { import = "nick.plugins" }, { import = "nick.plugins.lsp" } }, {
     change_detection = {
         notify = false,
     },
