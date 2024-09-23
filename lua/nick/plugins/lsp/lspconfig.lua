b3dcec4831a79088644f7677f85860201589ad77
Nicholas Stienz <nickstienz@gmail.com>
2024-09-23 10:02:25 -0500

    Got rid of random spaces -_-

--- lua/nick/plugins/lsp/lspconfig.lua
+++ lua/nick/plugins/lsp/lspconfig.lua
@@ -60,14 +60,14 @@ return {
         })
 
         local capabilities = cmp_nvim_lsp.default_capabilities()
-        
+
         mason_lspconfig.setup_handlers({
             function(server_name)
                 lspconfig[server_name].setup({
                     capabilities = capabilities,
                 })
             end,
-            
+
             ["lua_ls"] = function()
                 lspconfig["lua_ls"].setup({
                     capabilities = capabilities,
